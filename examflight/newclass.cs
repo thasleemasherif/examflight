5afbe25ab92c278f756e7ce8688324c34de737f6
thasleemasherif <thasleemasherif2256@gmail.com>
2021-12-12 12:43:46 +0530

    new file not added

---
+++ examflight/newclass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace examflight
+{
+    public class newclass
+    {
+        public int name { get; set; }
+        
+    }
+}
